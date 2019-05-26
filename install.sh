a536d8bb552fd18246a8ca0f30f8d93ae2ebcee4
R0flcopt3r <12752060+R0flcopt3r@users.noreply.github.com>
2019-05-26 17:36:39 +0200

    Complete rewrite

--- install.sh
+++ install.sh
@@ -1,84 +1,76 @@
 #!/bin/bash
 
+chassis=$(hostnamectl | grep "Chassis" | cut -d ':' -f2)
+
 if [[ -z $XDG_CONFIG_HOME ]]
 then
 	echo "XDG_CONFIG_HOME no set, using $HOME/.config"
 	XDG_CONFIG_HOME=$HOME/.config
 fi
 
+dot(){ # Path, name, hidden
 
-echo "Creating symlink for URxvt..."
-if [[ $(file $HOME/.Xresources | awk '{print$2}') -ne "symbolic" ]]
-then
-	echo "Xresouces exists as a regular file, deleting and linking"
-	rm $HOME/.Xresouces
-fi
-ln -s $(pwd)/urxvt/Xresources $HOME/.Xresources
+	if [ -h "$2" ]
+	then
+		printf "\t%s is symbolic, doing nothing\n" "$3"
+	elif [ -f "$2" ]
+	then
+		printf "\t%s a regular file, deleting...\n" "$3"
+		rm "$2"
+		printf "\tLinking\n"
+		ln -s "$1" "$2"
+		printf "\t%s is done\n" "$3"
+	else
+		printf "\tERROR: Can't determine.\n"
+		file "$1"
+	fi
 
-echo "Creating symlink for zsh..."
-if [[ $(file ~/.zshrc | awk '{print$2}') -eq "symbolic" ]]
-then
-	echo "Zshrc exists as a regular file, deleting and linking"
-	rm $HOME/.zshrc
-fi
-ln -s $(pwd)/zsh/zshrc $HOME/.zshrc
+}
+
+folder(){
+	if [[ ! -e $1 ]]
+	then
+		printf "\t%s does not exist, creating...\n" "$1"
+		mkdir "$1"
+	fi
+}
+
+# Initial checks for various config folders.
 echo "Installing oh-my-zsh"
-
 if [[ ! -e $HOME/.oh-my-zsh ]]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-echo "Creating symlink for neovim..."
-if [[ ! -e $XDG_CONFIG_HOME/nvim ]]
+printf "Checking and creating folders\n"
+folder "$XDG_CONFIG_HOME"/nvim
+folder "$XDG_CONFIG_HOME"/ranger
+folder "$XDG_CONFIG_HOME/"i3
+folder "$XDG_CONFIG_HOME"/i3blocks
+
+printf "Checking and copying files\n"
+dot "$(pwd)"/urxvt/Xresources 	"$HOME"/.Xresources						Xresources
+dot "$(pwd)"/zsh/zshrc 			"$HOME"/.zshrc							Zshrc
+dot "$(pwd)"/neovim/init.vim 	"$XDG_CONFIG_HOME"/nvim/init.vim		Init.vim
+dot "$(pwd)"/neovim/UltiSnips 	"$XDG_CONFIG_HOME"/nvim/UltiSnips		UltiSnips
+dot "$(pwd)"/neovim/spell 		"$XDG_CONFIG_HOME"/nvim/spell			Spelling
+dot "$(pwd)"/neovim/ftdetect 	"$XDG_CONFIG_HOME"/nvim/ftdetect		Filecheck
+dot "$(pwd)"/ranger/rc.conf 	"$XDG_CONFIG_HOME"/ranger/rc.conf 		rc.conf
+dot "$(pwd)"/ranger/rifle.conf 	"$XDG_CONFIG_HOME"/ranger/rifle.conf 	rifle
+dot "$(pwd)"/ranger/scope.sh 	"$XDG_CONFIG_HOME"/ranger/scope.sh 		scope
+dot "$(pwd)"/ranger/tagged 		"$XDG_CONFIG_HOME"/ranger/tagged 		tagged
+dot "$(pwd)"/ranger/bookmarks 	"$XDG_CONFIG_HOME"/ranger/bookmarks 	bookmarks
+
+printf "Checking and copying chassis specific configs\n"
+if [ "$chassis" == " desktop" ]
 then
-	echo "$XDG_CONFIG_HOME/nvim/ does not exist, creating.."
-	mkdir $XDG_CONFIG_HOME/nvim
+	dot "$(pwd)"/desktop/i3wm/config 	"$XDG_CONFIG_HOME"/i3/config 	i3wm
+	dot "$(pwd)"/desktop/i3blocks/config "$XDG_CONFIG_HOME"/i3blocks/config i3blocks
+elif [ "$chassis" == " laptop" ]
+then
+	dot "$(pwd)"/laptop/i3wm/config 	"$XDG_CONFIG_HOME"/i3/config 	i3wm
+	dot "$(pwd)"/laptop/i3blocks/config "$XDG_CONFIG_HOME"/i3blocks/config i3blocks
 fi
 
-	ln -s $(pwd)/neovim/init.vim $XDG_CONFIG_HOME/nvim/init.vim
-	ln -s $(pwd)/neovim/UltiSnips $XDG_CONFIG_HOME/nvim/UltiSnips
-	ln -s $(pwd)/neovim/spell $XDG_CONFIG_HOME/nvim/spell
-	ln -s $(pwd)/neovim/ftdetect $XDG_CONFIG_HOME/nvim/ftdetect
+dot "$(pwd)"/i3blocks/scripts "$XDG_CONFIG_HOME"/i3blocks/scripts i3blocksscripts
 
-
-echo "Creating symlink for i3WM..."
-if [[ ! -e $XDG_CONFIG_HOME/i3 ]]
-then
-	echo "$XDG_CONFIG_HOME/i3/ does not exist, creating.."
-	mkdir $XDG_CONFIG_HOME/i3
-fi
-if [[ $(file $XDG_CONFIG_HOME/i3/config | awk '{print$2}') -ne "symbolic" ]]
-then
-	echo "i3/config exists as a regular file, deleting and linking"
-	rm $XDG_CONFIG_HOME/i3/config
-fi
-ln -s $(pwd)/i3wm/config $XDG_CONFIG_HOME/i3/config
-
-echo "Creating symlink for i3blocks..."
-if [[ ! -e $XDG_CONFIG_HOME/i3blocks ]]
-then
-	echo "i3blocks/config exists as a regular file, deleting and linking"
-	mkdir $XDG_CONFIG_HOME/i3blocks
-fi
-if [[ $(file $XDG_CONFIG_HOME/i3/config | awk '{print$2}') -ne "symbolic" ]]
-then
-	echo "i3blocks/config exists as a regular file, deleting and linking"
-	rm $XDG_CONFIG_HOME/i3blocks/config
-fi
-ln -s $(pwd)/i3blocks/config $XDG_CONFIG_HOME/i3blocks/config
-
-echo "Creating symlink for Ranger..."
-if [[ ! -e $XDG_CONFIG_HOME/ranger ]]
-then
-	echo "i3/ranger exists as a regular file, deleting and linking"
-	mkdir $XDG_CONFIG_HOME/ranger
-fi
-ln -s $(pwd)/ranger/rc.conf $XDG_CONFIG_HOME/ranger/rc.conf
-ln -s $(pwd)/ranger/rifle.conf $XDG_CONFIG_HOME/ranger/rifle.conf
-ln -s $(pwd)/ranger/scope.sh $XDG_CONFIG_HOME/ranger/scope.sh
-ln -s $(pwd)/ranger/tagged $XDG_CONFIG_HOME/ranger/tagged
-ln -s $(pwd)/ranger/bookmarks $XDG_CONFIG_HOME/ranger/bookmarks
-
-
-xrdb $HOME/.Xresources
-
+xrdb "$HOME"/.Xresources
