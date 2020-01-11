a28a2353a243a5610d5ec99125f43c7b73bde3d1
R0flcopt3r <12752060+R0flcopt3r@users.noreply.github.com>
2020-01-11 14:13:58 +0100

    doom config changes

--- emacs_doom/config.el
+++ emacs_doom/config.el
@@ -3,6 +3,7 @@
 ;; Place your private configuration here
 
 (setq display-line-numbers-type 'relative)
+(setq-default tab-width 4)
 
 (setq doom-theme 'srcery)
 
@@ -21,5 +22,7 @@
 (after! evil-snipe
   (evil-snipe-mode -1))
 
+;;(add-to-list 'load-path "~/.emacs.d/emacs-application-framework")
+;;(require 'eaf)
 
 (atomic-chrome-start-server)
