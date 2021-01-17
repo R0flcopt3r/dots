27f990ab1c6434349120a27f2f1b324156b9870d
R0flcopt3r <12752060+R0flcopt3r@users.noreply.github.com>
2021-01-17 21:04:56 +0100

    fix(doom): removing +tng on company

--- emacs_doom/init.el
+++ emacs_doom/init.el
@@ -13,8 +13,7 @@
        ;;japanese
 
        :completion
-       (company           ; the ultimate code completion backend
-        +tng)
+       company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (ivy               ; a search engine for love and life
