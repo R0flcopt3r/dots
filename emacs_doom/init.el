ab83dc6934f75509aab513a76349e391f370509e
R0flcopt3r <12752060+R0flcopt3r@users.noreply.github.com>
2021-01-17 21:05:58 +0100

    feat(doom): poetry and pyright for python

--- emacs_doom/init.el
+++ emacs_doom/init.el
@@ -155,7 +155,9 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python            ; beautiful is better than ugly
+        +poetry
         +pyenv
+        +pyright
         +lsp)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
