97ae4d02750c680eacf58a0de245a8dae2519fc7
R0flcopt3r <12752060+R0flcopt3r@users.noreply.github.com>
2021-01-17 21:03:25 +0100

    feat(doom): python specific config and DAP

--- emacs_doom/config.el
+++ emacs_doom/config.el
@@ -68,3 +68,53 @@
 
 
 (load! "~/.doom.d/mail.el")
+
+(after! poetry
+ (map! :map python-mode-map
+      :leader
+      :desc "poetry run" "pc"
+      #'poetry-run)
+ (map! :map python-mode-map
+       :localleader
+       :desc "poetry" "p"
+       #'poetry))
+
+;; Found on random website
+;; (after! (python flycheck)
+;;     (add-hook 'flycheck-mode-hook #'flycheck-pycheckers-setup)
+;;     (setq flycheck-pycheckers-checkers '(mypy pyflakes)))
+
+(after! dap-mode
+ (require 'dap-python)
+ (setq dap-python-executable "python3"
+       dap-python-debugger 'debugpy)
+ (map! :map dap-mode-map
+       :leader
+       :prefix ("d" . "dap")
+       ;; basics
+       :desc "dap next"          "n" #'dap-next
+       :desc "dap step in"       "i" #'dap-step-in
+       :desc "dap step out"      "o" #'dap-step-out
+       :desc "dap continue"      "c" #'dap-continue
+       :desc "dap hydra"         "h" #'dap-hydra
+       :desc "dap debug restart" "r" #'dap-debug-restart
+       :desc "dap debug"         "s" #'dap-debug
+
+       ;; debug
+       :prefix ("dd" . "Debug")
+       :desc "dap debug recent"  "r" #'dap-debug-recent
+       :desc "dap debug last"    "l" #'dap-debug-last
+
+       ;; eval
+       :prefix ("de" . "Eval")
+       :desc "eval"                "e" #'dap-eval
+       :desc "eval region"         "r" #'dap-eval-region
+       :desc "eval thing at point" "s" #'dap-eval-thing-at-point
+       :desc "add expression"      "a" #'dap-ui-expressions-add
+
+       ;; breakpoints
+       :prefix ("db" . "Breakpoint")
+       :desc "dap breakpoint toggle"      "b" #'dap-breakpoint-toggle
+       :desc "dap breakpoint condition"   "c" #'dap-breakpoint-condition
+       :desc "dap breakpoint hit count"   "h" #'dap-breakpoint-hit-condition
+       :desc "dap breakpoint log message" "l" #'dap-breakpoint-log-message))
