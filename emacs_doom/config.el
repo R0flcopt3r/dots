76edccfa3c8a2c0deef36491b6c779d6f45cf565
Eirik Osland Lavik <eirik@Eiriks-MBP.appeartv.lan>
2021-09-08 09:48:50 +0200

    doom: change themes when system changes mac only

--- emacs_doom/config.el
+++ emacs_doom/config.el
@@ -18,6 +18,15 @@
 
 (display-time)
 
+(defun my/apply-theme (appearance)
+  "Load theme, taking current system APPEARANCE into consideration."
+  (mapc #'disable-theme custom-enabled-themes)
+  (pcase appearance
+    ('light (load-theme 'tango t))
+    ('dark (load-theme 'srcery t))))
+
+(when IS-MAC
+  (add-hook 'ns-system-appearance-change-functions #'my/apply-theme))
 
 (setq lsp-clients-clangd-args '("-j=3"
                                 "--background-index"
